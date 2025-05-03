0bef91e03e0e799ec923ebe9b733d3b0e927ded4
markabayon <mark.abayon@dict.gov.ph>
2025-05-04 01:03:01 +0800

    saved

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const studentRoutes = require("./routes/studentRoutes");
-const StudentFamilyBackgroundRoutes = require("./routes/studentFamilyBackgroundRoutes");
-const StudentCoursesRoutes = require("./routes/studentCoursesRoutes");
-const CoursesRoutes = require("./routes/coursesRoutes");
-const CredentialsRoutes = require("./routes/credentialsRoutes");
-const EmployeeRoutes = require("./routes/employeeRoutes");
-const AverageGradeRoutes = require("./routes/averageGradeRoutes");
-const OtherInformationRoutes = require("./routes/otherInformationRoutes");
-const AnnouncementRoutes = require("./routes/announcementRoutes");
-const CommentsRoutes = require("./routes/commentsRoutes");
-const ReactionRoutes = require("./routes/reactionRoutes");
-const authRoutes = require("./routes/authRoutes");
-const authUserRoutes = require("./routes/authUserRoutes");
-const Messages = require("./routes/messagesRoutes");
-const StudentFriend = require("./routes/studentFriendRoutes");
+
 
 const app = express();
 app.use(express.json()); // For parsing JSON request bodies
