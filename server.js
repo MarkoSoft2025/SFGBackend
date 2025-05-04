0821c08a706156ed0321b561ab39962c73742bee
markabayon <mark.abayon@dict.gov.ph>
2025-05-04 08:44:19 +0800

    Sve

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const studentRoutes = require("./routes/studentRoutes");
-
+const employeeRoutes = require("./routes/employeeRoutes");
 
 const app = express();
 app.use(express.json()); // For parsing JSON request bodies
@@ -10,6 +10,7 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing
 // Routes
 
 app.use("/api/Students", studentRoutes);
+app.use("/api/Employees", employeeRoutes);
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the API!" });
